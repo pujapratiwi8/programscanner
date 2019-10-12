294b46ddaa98fd5681c15f33b5e7bce13ee0c6fd
pujapratiwi8 <phujapratiwi777@gmail.com>
2019-10-12 19:50:14 -0400

    Update Latihan1.java

--- src/latihan1/Latihan1.java
+++ src/latihan1/Latihan1.java
@@ -1,54 +1,31 @@
 package latihan1;
+
 import java.util.Scanner;
 
 public class Latihan1 {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        String userName;
-        String asal;
-        int umur;
-        int biayakos;
-        
-        
-        System.out.println ("selamat datang dikos widya 1,");
-        System.out.print ("nama anda ?");
+        Scanner sc = new Scanner(System.in);
+        String userName, asal;
+        int umur, biayakos;
+
+        System.out.println("selamat datang dikos widya 1,");
+        System.out.print("nama anda ?");
         userName = sc.nextLine();
-        
-        System.out.print ("asal dari mana ?");
+
+        System.out.print("asal dari mana ?");
         asal = sc.nextLine();
-        
-        System.out.print ("umur: ");
+
+        System.out.print("umur: ");
         umur = sc.nextInt();
-        
-        System.out.print ("biaya kos :");
+
+        System.out.print("biaya kos :");
         biayakos = sc.nextInt();
-        
+
         //menampilan yang sudah disimpan divariabel
         System.out.println("-------------------------------");
-        System.out.println ("nama :" + userName);
-        System.out.println ("asal dari: " + asal);
-        System.out.println ("umur:" + umur);
-        System.out.println ("biayakos:" + biayakos);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
-        
-        
-        
-        
+        System.out.println("nama :" + userName);
+        System.out.println("asal dari: " + asal);
+        System.out.println("umur:" + umur);
+        System.out.println("biaya kos:" + biayakos);
     }
-    
 }
